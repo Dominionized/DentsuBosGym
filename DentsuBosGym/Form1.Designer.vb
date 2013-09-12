da8da3648638532d745208fa5e85087900fb9216
Lamperouge <infinityninji@gmail.com>
2013-09-12 12:51:23 -0400

    rajouté bouton

--- DentsuBosGym/Form1.Designer.vb
+++ DentsuBosGym/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class frmInscription
     Private Sub InitializeComponent()
         Me.picCoin = New System.Windows.Forms.PictureBox()
         Me.lblTitre = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.picCoin, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -47,12 +48,22 @@ Partial Class frmInscription
         Me.lblTitre.TabIndex = 1
         Me.lblTitre.Text = "DentsuBosGym"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(238, 159)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmInscription
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Control
         Me.ClientSize = New System.Drawing.Size(384, 561)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.lblTitre)
         Me.Controls.Add(Me.picCoin)
         Me.ForeColor = System.Drawing.Color.Black
@@ -67,5 +78,6 @@ Partial Class frmInscription
     End Sub
     Friend WithEvents picCoin As System.Windows.Forms.PictureBox
     Friend WithEvents lblTitre As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
