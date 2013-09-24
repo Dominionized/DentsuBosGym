6bc767fa709351c770bd3a95255e6b5acb52db16
Lamperouge <infinityninji@gmail.com>
2013-09-24 09:24:53 -0400

    bacon

--- DentsuBosGym/frmFacture.Designer.vb
+++ DentsuBosGym/frmFacture.Designer.vb
@@ -22,8 +22,41 @@ Partial Class frmFacture
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txtFacture = New System.Windows.Forms.TextBox()
+        Me.btnFermerFacture = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFacture
+        '
+        Me.txtFacture.Location = New System.Drawing.Point(13, 13)
+        Me.txtFacture.Multiline = True
+        Me.txtFacture.Name = "txtFacture"
+        Me.txtFacture.ReadOnly = True
+        Me.txtFacture.Size = New System.Drawing.Size(259, 207)
+        Me.txtFacture.TabIndex = 0
+        '
+        'btnFermerFacture
+        '
+        Me.btnFermerFacture.Location = New System.Drawing.Point(197, 226)
+        Me.btnFermerFacture.Name = "btnFermerFacture"
+        Me.btnFermerFacture.Size = New System.Drawing.Size(75, 23)
+        Me.btnFermerFacture.TabIndex = 1
+        Me.btnFermerFacture.Text = "Fermer"
+        Me.btnFermerFacture.UseVisualStyleBackColor = True
+        '
+        'frmFacture
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnFermerFacture)
+        Me.Controls.Add(Me.txtFacture)
+        Me.Name = "frmFacture"
         Me.Text = "frmFacture"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents txtFacture As System.Windows.Forms.TextBox
+    Friend WithEvents btnFermerFacture As System.Windows.Forms.Button
 End Class
